9b9742f2f7cf11eaa0139bc00c64906d0920b98a
Ezequiel <ezequiel.domenech@gmail.com>
2018-11-23 13:35:17 -0300

    commit inicial

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop_base',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'Lz3BQTkcchspJPXVLzjE7mFfK409yc9kmAAViG3MQBD9q1216DLjaU67',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-11-23',
+    'locale' => 'es-ES',
+    'cookie_key' => 'fHccodFKs6XAkisL6HnIR8ZyMm2khOveBdW8yCEqua1yzYm5hCV5FzwX',
+    'cookie_iv' => 'Z263P90j',
+    'new_cookie_key' => 'def00000199644a4918614b6ce652b196c5f1d2b3c65990757c83c3bc40347a04baccd4642481e8a57351b0784003244f265e91933aaf44d747d8764f1b661ce19e2decc',
+  ),
+);
